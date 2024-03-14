c58414a4607424f7764f676e559c738bc4ad6b96
Engelbrecht Jerome <c1b3rjerome@gmail.com>
2024-03-14 07:55:10 +0100

    merge

--- Web_Gun_Rerun/Web/www/src/login-validation.js
+++ Web_Gun_Rerun/Web/www/src/login-validation.js
@@ -1,17 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('login-form');
+    var errorMessage = document.getElementById("confirmError");
+    var successMessage = document.getElementById("confirmSuccess");
 
     form.addEventListener('submit', function (event) {
         const isFormValid = validateLoginForm();
-        
+
         if (!isFormValid) {
             event.preventDefault(); // Prevent form submission if validation fails
+        } else {
+
+
+            const username = document.getElementById("loginUsername").value.trim();
+            const password = document.getElementById("loginPassword").value.trim();
+            
+            const xhr = new XMLHttpRequest();
+            const url = '../htbin/login.py';
+
+            const data = new FormData();
+            data.append('username', username);
+            data.append('userpwd', password);
+
+            console.log(username);
+
+            xhr.open('POST', url, true);
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    if(xhr.responseText[0] ==='B'){
+                        successMessage.textContent = xhr.responseText;
+                    }
+                    else{
+                        errorMessage.textContent = xhr.responseText;
+                    }
+
+                }
+            };
+
+            xhr.send(data);
+            
+            event.preventDefault();
         }
     });
 });
 
 function validateLoginForm() {
-    return validateLoginPassword() && validateLoginUsername();
+    const isUsernameValid = validateLoginUsername();
+    const isPasswordValid = validateLoginPassword();
+
+    return isUsernameValid && isPasswordValid;
 }
 
 function validateLoginUsername() {
@@ -26,9 +63,10 @@ function validateLoginUsername() {
     }
 }
 
-function validateLoginPassword(password, errorElement) {
-    var password = document.getElementById("loginPassword").value.trim();
-    var passwordError = document.getElementById("loginPasswordError");
+function validateLoginPassword() {
+    const password = document.getElementById("loginPassword").value.trim();
+    const passwordError = document.getElementById("loginPasswordError");
+
     if (password === "") {
         passwordError.textContent = "Please enter your password.";
         return false;
